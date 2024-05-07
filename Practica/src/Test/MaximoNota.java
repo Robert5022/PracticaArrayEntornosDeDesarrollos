4f6431b6cb811fdf624f6d70864abf295fde2e1f
unknown <Robert5022>
2024-05-07 17:45:05 +0200

    Maximo

---
+++ Practica/src/Test/MaximoNota.java
@@ -0,0 +1,19 @@
+package Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MaximoNota {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
